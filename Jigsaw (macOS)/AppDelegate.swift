628f22b914c0a3ed340257a91a02e85e1b4ce6d1
Simon Fairbairn <simon@line-in.co.uk>
2018-07-13 18:38:24 +0100

    Adding assets

---
+++ Jigsaw (macOS)/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Jigsaw (macOS)
+//
+//  Created by Simon Fairbairn on 13/07/2018.
+//  Copyright © 2018 Voyage Travel Apps. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
