5decd75264dfc57f096324736e6d4e43890feb15
Simon Fairbairn <simon@line-in.co.uk>
2018-08-15 13:30:29 +0100

    Adding the basic scale component

---
+++ Shared/ScaleComponent.swift
@@ -0,0 +1,16 @@
+//
+//  ScaleComponent.swift
+//  Jigsaw
+//
+//  Created by Simon Fairbairn on 15/08/2018.
+//  Copyright © 2018 Voyage Travel Apps. All rights reserved.
+//
+
+import GameplayKit
+import SpriteKit
+
+class ScaleComponent : GKComponent {
+	let scaleAmountPerSecond : CGFloat = 0.01
+	var targetScale : CGFloat = 1
+}
+
